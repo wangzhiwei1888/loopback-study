38d4200df2f576260878f5666a6a1dda7277cff1
zhiweiwang6 <zhiweiwang6@creditease.cn>
2016-07-27 10:12:51 +0800

    add mongodb and add dev

---
+++ server/datasources.dev.js
@@ -0,0 +1,10 @@
+
+exports.mongodb = {
+	"host": "localhost",
+    "port": 27017,
+    "database": "loopback-dev",
+    "password": "",
+    "name": "mongodb",
+    "user": "",
+    "connector": "mongodb"
+}
